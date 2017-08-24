2944c3ec6a72fb25fa93dfefcbd509ff728b4c00
GaopanHuang <huanggpan@163.com>
2017-08-24 11:38:42 +0800

    multigpu training for vgg and resnet

--- resnet50formnist.py
+++ resnet50formnist.py
@@ -1,3 +1,21 @@
+import os
+#gpu_id = '1,2'
+gpu_id = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
+os.system('echo $CUDA_VISIBLE_DEVICES')
+
+import tensorflow as tf
+#config = tf.ConfigProto(log_device_placement=True, allow_soft_placement=True)
+config = tf.ConfigProto(allow_soft_placement=True)
+config.gpu_options.allow_growth = True
+#config.gpu_options.per_process_gpu_memory_fraction = 0.4
+
+sess = tf.Session(config=config)
+
+from keras import backend as K
+K.set_session(sess)
+
+
 import keras
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
@@ -5,7 +23,6 @@ from keras.models import Model
 from keras.callbacks import TensorBoard
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 from keras.layers import Dense, GlobalAveragePooling2D
-from keras import backend as K
 import numpy as np
 
 train_num = 32000
@@ -27,7 +44,7 @@ model = Model(inputs=base_model.input, outputs=predictions)
 #model.summary()
 
 #from keras.utils import plot_model
-#plot_model(model, to_file='model.png')
+#plot_model(model, to_file='modelresnet50.png')
 
 # first: train only the top layers (which were randomly initialized)
 # i.e. freeze all convolutional InceptionV3 layers
@@ -35,49 +52,54 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # compile the model (should be done *after* setting layers to non-trainable)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+    metrics=['accuracy'])
 
 def resizeimg(arr):
-    a = arr.reshape((28,28))
-    a = a[:,:,np.newaxis]
-    img = image.array_to_img(a)
-    hw_tuple = (224,224)
-    img = img.resize(hw_tuple)
-    # need to add grayimg to rgb
-    img = img.convert("RGB")
-    x = image.img_to_array(img)                                   
-    x = np.expand_dims(x, axis=0)                                 
+    a = arr.reshape((-1,28,28))
+    a = a[:,:,:,np.newaxis]
+    x = np.empty(shape=[0,224,224,3])
+    for i in range(len(a)):
+        img = a[i,:,:,:]
+        img = image.array_to_img(img)
+        hw_tuple = (224,224)
+        img = img.resize(hw_tuple)
+        # need to add grayimg to rgb
+        img = img.convert("RGB")
+        x1 = image.img_to_array(img)
+        x1 = x1[np.newaxis,:,:,:]
+        x = np.append(x, x1, axis=0)
     return preprocess_input(x)
 
-def generate_train(path):
+def generate_train(path, batch_size):
     while 1:
         samples = np.loadtxt(open("train.csv","rb"),delimiter=",",skiprows=1)
-        for i in range(train_num):
+        steps = train_num/batch_size
+        for i in range(steps):
             # create numpy arrays of input data
             # and labels, from each line in the file
-            x = resizeimg(samples[i,1:])
-            y = keras.utils.to_categorical(samples[i,0], num_classes=10)
+            x = resizeimg(samples[i*batch_size:(i+1)*batch_size,1:])
+            y = keras.utils.to_categorical(samples[i*batch_size:(i+1)*batch_size,0], num_classes=10)
             #yield ({'input_1': x}, {'output': y})
             yield (x, y)
-        f.close()
 
-def generate_val(path):
+def generate_val(path, batch_size):
     while 1:
         samples = np.loadtxt(open("train.csv","rb"),delimiter=",",skiprows=train_num+1)
-        for i in range(len(samples)):
+        steps = len(samples)/batch_size
+        for i in range(steps):
             # create numpy arrays of input data
             # and labels, from each line in the file
-            x = resizeimg(samples[i,1:])
-            y = keras.utils.to_categorical(samples[i,0], num_classes=10)
+            x = resizeimg(samples[i*batch_size:(i+1)*batch_size,1:])                                                                              
+            y = keras.utils.to_categorical(samples[i*batch_size:(i+1)*batch_size,0], num_classes=10)                                              
             #yield ({'input_1': x}, {'output': y})
             yield (x, y)
-        f.close() 
 
-tensorboard = TensorBoard(log_dir='./logs')
+tensorboard = TensorBoard(log_dir='./resnetlogs')
 
-model.fit_generator(generator=generate_train('train.csv'),steps_per_epoch=1000,
-    epochs=10,callbacks=[tensorboard], 
-    validation_data=generate_val('train.csv'), validation_steps=100)
+model.fit_generator(generator=generate_train('train.csv',32),steps_per_epoch=1000,
+    epochs=30,callbacks=[tensorboard], 
+    validation_data=generate_val('train.csv',100), validation_steps=100)
 
 '''
 img_path = 'elephant.jpg'
@@ -85,7 +107,6 @@ img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
-
 preds = model.predict(x)
 # decode the results into a list of tuples (class, description, probability)
 # (one such list for each sample in the batch)
--- vgg16formnist.py
+++ vgg16formnist.py
@@ -1,3 +1,22 @@
+import os
+#gpu_id = '1,2'
+gpu_id = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
+os.system('echo $CUDA_VISIBLE_DEVICES')
+
+import tensorflow as tf
+#config = tf.ConfigProto(log_device_placement=True, allow_soft_placement=True)
+config = tf.ConfigProto(allow_soft_placement=True)
+config.gpu_options.allow_growth = True
+#config.gpu_options.per_process_gpu_memory_fraction = 0.4
+
+sess = tf.Session(config=config)
+
+from keras import backend as K
+K.set_session(sess)
+
+
+
 import keras
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
@@ -33,54 +52,61 @@ plot_model(model, to_file='model_vgg16formnist.png')
 
 # first: train only the top layers (which were randomly initialized)
 # i.e. freeze all convolutional InceptionV3 layers
-#for layer in base_model.layers:
-#    layer.trainable = False
-for layer in model.layers[:11]:
-   layer.trainable = False
-for layer in model.layers[11:]:
-   layer.trainable = True
+for layer in base_model.layers:
+    layer.trainable = False
+#for layer in model.layers[:11]:
+#   layer.trainable = False
+#for layer in model.layers[11:]:
+#   layer.trainable = True
 
 # compile the model (should be done *after* setting layers to non-trainable)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+    metrics=['accuracy'])
+
 
 def resizeimg(arr):
-    a = arr.reshape((28,28))
-    img = pilimage.fromarray(a)
-    img = img.convert("RGB")
-    img = img.resize((224,224),pilimage.ANTIALIAS)
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)                                 
+    a = arr.reshape((-1,28,28))
+    a = a[:,:,:,np.newaxis]
+    x = np.empty(shape=[0,224,224,3])
+    for i in range(len(a)):
+        img = a[i,:,:,:]
+        img = image.array_to_img(img)
+        hw_tuple = (224,224)
+        img = img.resize(hw_tuple)
+        # need to add grayimg to rgb
+        img = img.convert("RGB")
+        x1 = image.img_to_array(img)
+        x1 = x1[np.newaxis,:,:,:]
+        x = np.append(x, x1, axis=0)
     return preprocess_input(x)
 
-def generate_train(path):
+def generate_train(path, batch_size):
     while 1:
         samples = np.loadtxt(open("train.csv","rb"),delimiter=",",skiprows=1)
-        for i in range(train_num):
+        steps = train_num/batch_size
+        for i in range(steps):
             # create numpy arrays of input data
             # and labels, from each line in the file
-            x = resizeimg(samples[i,1:])
-            y = keras.utils.to_categorical(samples[i,0], num_classes=10)
-            #yield ({'input_1': x}, {'output': y})
+            x = resizeimg(samples[i*batch_size:(i+1)*batch_size,1:])
+            y = keras.utils.to_categorical(samples[i*batch_size:(i+1)*batch_size,0], num_classes=10)
             yield (x, y)
-        f.close()
 
-def generate_val(path):
+def generate_val(path, batch_size):
     while 1:
         samples = np.loadtxt(open("train.csv","rb"),delimiter=",",skiprows=train_num+1)
-        for i in range(len(samples)):
+        steps = len(samples)/batch_size
+        for i in range(steps):
             # create numpy arrays of input data
             # and labels, from each line in the file
-            x = resizeimg(samples[i,1:])
-            y = keras.utils.to_categorical(samples[i,0], num_classes=10)
-            #yield ({'input_1': x}, {'output': y})
+            x = resizeimg(samples[i*batch_size:(i+1)*batch_size,1:])                                                                              
+            y = keras.utils.to_categorical(samples[i*batch_size:(i+1)*batch_size,0], num_classes=10)                                              
             yield (x, y)
-        f.close() 
 
-tensorboard = TensorBoard(log_dir='./logs')
+tensorboard = TensorBoard(log_dir='./vgglogs')
 
-model.fit_generator(generator=generate_train('train.csv'),steps_per_epoch=1000,
-    epochs=100,callbacks=[tensorboard], 
-    validation_data=generate_val('train.csv'), validation_steps=100)
+model.fit_generator(generator=generate_train('train.csv',32),steps_per_epoch=1000,
+    epochs=50,callbacks=[tensorboard], 
+    validation_data=generate_val('train.csv',100), validation_steps=100)
 
 '''
 img_path = 'elephant.jpg'
@@ -88,10 +114,10 @@ img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
-
 preds = model.predict(x)
 # decode the results into a list of tuples (class, description, probability)
 # (one such list for each sample in the batch)
 print('Predicted:', decode_predictions(preds, top=3)[0])
 '''
 # Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
+
